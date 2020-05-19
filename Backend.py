69acb60734fd0cf89b4af2aead92d12b1d64dc32
GokayY <mu2phy@gmail.com>
2020-05-19 14:10:42 +0100

    DB file and fix

--- Backend.py
+++ Backend.py
@@ -1,25 +1,27 @@
 import sqlite3 as sql
+import os.path
 
-
+base_dir = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(base_dir, "books.db")
 
 def connect():
     conn = sql.connect("books.db")
     cur = conn.cursor()
-    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
+    cur.execute("CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
     conn.commit()
     conn.close()
 
 def insert(title, author, year, isbn):
     conn = sql.connect("books.db")
     cur = conn.cursor()
-    cur.execute("INSERT INTO book Values (NULL, ?, ?, ?, ?)",(title, author, year, isbn))
+    cur.execute("INSERT INTO books Values (NULL, ?, ?, ?, ?)",(title, author, year, isbn))
     conn.commit()
     conn.close()
 
 def view():
     conn = sql.connect("books.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM book")
+    cur.execute("SELECT * FROM books")
     rows = cur.fetchall()
     conn.close()
     return rows
@@ -27,7 +29,7 @@ def view():
 def search(title="", author="", year="", isbn=""):
     conn = sql.connect("books.db")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM book WHERE title=? OR author=? or year=? OR isbn=?", (title, author, year,isbn))
+    cur.execute("SELECT * FROM books WHERE title=? OR author=? or year=? OR isbn=?", (title, author, year,isbn))
     rows = cur.fetchall()
     conn.close()
     return rows
@@ -35,13 +37,15 @@ def search(title="", author="", year="", isbn=""):
 def delete(id):
     conn = sql.connect("books.db")
     cur = conn.cursor()
-    cur.execute("DELETE FROM book WHERE id=?", (id,))
+    cur.execute("DELETE FROM books WHERE id=?", (id,))
     conn.commit()
     conn.close()
 
 def update(id,title,author,year,isbn):
     conn = sql.connect("books.db")
     cur = conn.cursor()
-    cur.execute("UPDATE book SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))
+    cur.execute("UPDATE books SET title=?, author=?, year=?, isbn=? WHERE id=?", (title,author,year,isbn,id))
     conn.commit()
     conn.close()
+
+connect()
